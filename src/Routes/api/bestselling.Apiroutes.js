9c5320629f1c7e7dd61f053a897bd24796d37389
Arop Sutra Dhar <142885013+aropdhar@users.noreply.github.com>
2025-01-01 15:05:19 +0600

    bestselling_update

--- src/Routes/api/bestselling.Apiroutes.js
+++ src/Routes/api/bestselling.Apiroutes.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const {Router} = express;
 const _ = Router();
-const {bestsellingcontroller} = require('../../controller/bestsellingproduct.controller')
+const {bestsellingcontroller , bestSellingGetAllProduct , UpdatebestSellingProduct , deletebestsellingproduct} = require('../../controller/bestsellingproduct.controller')
 
-_.route("/bestsellingproduct").post(bestsellingcontroller)
+_.route("/bestsellingproduct").post(bestsellingcontroller).get(bestSellingGetAllProduct);
+
+_.route("/bestsellingproduct/:productId").put(UpdatebestSellingProduct).delete(deletebestsellingproduct)
 
 module.exports = _;
--- src/controller/bestsellingproduct.controller.js
+++ src/controller/bestsellingproduct.controller.js
@@ -2,12 +2,110 @@ const {apiError} = require('../utils/apiError.js');
 const {apiResponse} = require('../utils/apiResonse.js');
 const {bestsellingModel} = require('../Model/bestSelling.model.js');
 
+/**
+ * product controller Create
+ * @param {*} req 
+ * @param {*} res 
+ * @returns 
+ */
+
 const bestsellingcontroller = async(req,res)=>{
     try {
-        console.log("arop Dhar");
+        const {product} = req?.body;
+        if(!product){
+            return res.status(400).json(new apiError(false , null , 404 , `product missing!!`))
+        }
+        
+        // This Product Already Exist in database
+
+        const productAlreadyExist = await bestsellingModel.find({product: product});
+
+        if(productAlreadyExist?.length){
+            return res.status(400).json(new apiError(false , null , 404 , `${product} Already Exist!!`))
+        }
+
+        // now database save
+
+        const bestSellingSave = await bestsellingModel({product}).save()
+
+        if(bestSellingSave){
+            return res.status(200).json(new apiResponse(true , bestSellingSave, 200 , null , "bestSellingProDuct Create  Successfully!!"))
+        }
+
     } catch (error) {
         return res.status(400).json(new apiError(false , null , 404 , `bestselling controller Error: ${error}`))
     }
 }
 
-module.exports = {bestsellingcontroller}
+/**
+ * Get All Best Selling Product
+*/
+
+
+const bestSellingGetAllProduct =  async(req, res)=>{
+    try {
+ 
+        const GetAllbestsellingproduct = await bestsellingModel.find({}).populate("product");
+
+        if(GetAllbestsellingproduct){
+            return res.status(200).json(new apiResponse(true , GetAllbestsellingproduct, 200 , null , "Get All bestsellingproduct Successfully!!"))
+        }else{
+            return res.status(400).json(new apiError(false , null , 404 , `bestselling product Not Found`))
+        }
+
+        
+    } catch (error) {
+        return res.status(400).json(new apiError(false , null , 404 , `bestSellingGetAllProduct controller Error: ${error}`))
+    }
+}
+
+/**
+ * Get All Best Selling Product
+*/
+
+const UpdatebestSellingProduct =  async(req,res)=>{
+    try {
+        const {productId} = req?.params;
+
+        const updatebestselling = await bestsellingModel.findOneAndUpdate(
+            {product: productId},
+            {...req.body},
+            {new: true}
+        );
+
+        if(updatebestselling){
+            return res.status(200).json(new apiResponse(true , updatebestselling, 200 , null , "update bestselling product Successfully!!"))
+        }else{
+            return res.status(400).json(new apiError(false , null , 404 , `bestselling Product Updated Failed`))
+        }
+    
+        
+    } catch (error) {
+        return res.status(400).json(new apiError(false , null , 404 , `UpdatebestSellingProduct controller Error: ${error}`))
+    }
+}
+
+/**
+ * Delete Best Selling Product
+*/
+
+const deletebestsellingproduct = async(req,res)=>{
+   try {
+     
+    const {productId} = req.params;
+
+    const deletebestselling = await bestsellingModel.findOneAndDelete({product: productId});
+
+    if(deletebestselling){
+        return res.status(200).json(new apiResponse(true , deletebestselling, 200 , null , "delete bestselling product Successfully!!"))
+    }else{
+        return res.status(400).json(new apiError(false , null , 404 , `bestselling delete failed`))
+    }
+
+    
+   } catch (error) {
+    return res.status(400).json(new apiError(false , null , 404 , `delete bestselling product controller Error: ${error}`))
+   }
+}
+
+module.exports = {bestsellingcontroller , bestSellingGetAllProduct , UpdatebestSellingProduct , deletebestsellingproduct}
